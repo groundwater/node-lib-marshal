7b9008b256e7aa729877888f3979acd17437fc84
Jacob Groundwater <jacob@newrelic.com>
2014-04-02 21:10:53 -0700

    class type should not require specific type
    you can use any object that satisfies the required interface

--- index.js
+++ index.js
@@ -64,36 +64,33 @@ MapType.prototype.ratify = function (val) {
   return out;
 };
 
-function ClassType(type) {
-  this.type = type;
-  this.types = {};
-  this.optn = {};
+function ClassType() {
+  this.required = {};
+  this.optional = {};
 }
 
 ClassType.prototype.addRequired = function (key, type) {
-  this.types[key] = type;
+  this.required[key] = type;
 
   return this;
 };
 
 ClassType.prototype.addOptional = function (key, type) {
-  this.optn[key] = type;
+  this.optional[key] = type;
 
   return this;
 };
 
 ClassType.prototype.ratify = function (val) {
-  if (!(val instanceof this.type)) throw new Error();
-
   var out = {};
-  Object.keys(this.types).forEach(function (key) {
-    out[key] = this.types[key].ratify(val[key]);
+  Object.keys(this.required).forEach(function (key) {
+    out[key] = this.required[key].ratify(val[key]);
   }, this);
 
-  Object.keys(this.optn).forEach(function (key) {
+  Object.keys(this.optional).forEach(function (key) {
     if (val[key] === undefined) return;
 
-    out[key] = this.optn[key].ratify(val[key]);
+    out[key] = this.optional[key].ratify(val[key]);
   }, this);
 
   return out;
