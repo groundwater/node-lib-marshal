2855efc4cca2d803d7984db911df6d66f99aa41b
Jacob Groundwater <jacob@newrelic.com>
2014-04-02 20:11:49 -0700

    extract and create tap tests

---
+++ index.js
@@ -0,0 +1,97 @@
+/* jshint latedef: false */
+module.exports = {
+  NumberType : NumberType,
+  StringType : StringType,
+  ArrayType  : ArrayType,
+  MapType    : MapType,
+  ClassType  : ClassType,
+};
+
+function NumberType(opts) {
+  this.opts = opts;
+}
+
+NumberType.prototype.parse = function (val) {
+  if (typeof val !== 'number') throw new Error();
+
+  return val;
+};
+
+function StringType(opts) {
+  this.opts = opts;
+}
+
+StringType.prototype.parse = function (val) {
+  if (typeof val !== 'string') throw new Error();
+
+  return val;
+};
+
+function ArrayType(type) {
+  this.type = type;
+}
+
+ArrayType.prototype.parse = function (val) {
+  if (! Array.isArray(val)) throw new Error();
+
+  var type = this.type;
+  var out = [];
+
+  val.forEach(function (item) {
+    out.push(type.parse(item));
+  });
+
+  return out;
+};
+
+function MapType(type) {
+  this.type = type;
+}
+
+MapType.prototype.parse = function (val) {
+  var type  = this.type;
+
+  if (typeof val !== 'object') throw new Error();
+
+  var out = {};
+  Object.keys(val).forEach(function (key) {
+    out[key] = type.parse(val[key]);
+  });
+
+  return out;
+};
+
+function ClassType(type) {
+  this.type = type;
+  this.types = {};
+  this.optn = {};
+}
+
+ClassType.prototype.addRequired = function (key, type) {
+  this.types[key] = type;
+
+  return this;
+};
+
+ClassType.prototype.addOptional = function (key, type) {
+  this.optn[key] = type;
+
+  return this;
+};
+
+ClassType.prototype.parse = function (val) {
+  if (!(val instanceof this.type)) throw new Error();
+
+  var out = {};
+  Object.keys(this.types).forEach(function (key) {
+    out[key] = this.types[key].parse(val[key]);
+  }, this);
+
+  Object.keys(this.optn).forEach(function (key) {
+    if (val[key] === undefined) return;
+
+    out[key] = this.optn[key].parse(val[key]);
+  }, this);
+
+  return out;
+};
--- test/other.js
+++ test/other.js
@@ -1,142 +1,78 @@
-function NumberType(opts) {
-  this.opts = opts;
-}
+var proto = require('../index.js');
+var test = require('tap').test;
 
-NumberType.prototype.parse = function (val) {
-  if (typeof val !== 'number') throw new Error();
-
-  return val;
-};
-
-function StringType(opts) {
-  this.opts = opts;
-}
-
-StringType.prototype.parse = function (val) {
-  if (typeof val !== 'string') throw new Error();
-
-  return val;
-};
-
-function ArrayType(type) {
-  this.type = type;
-}
-
-ArrayType.prototype.parse = function (val) {
-  if (! Array.isArray(val)) throw new Error();
-
-  var type = this.type;
-  var out = [];
-
-  val.forEach(function (item) {
-    out.push(type.parse(item));
-  });
-
-  return out;
-};
-
-function MapType(type) {
-  this.type = type;
-}
-
-MapType.prototype.parse = function (val) {
-  var type  = this.type;
-
-  if (typeof val !== 'object') throw new Error();
-
-  var out = {};
-  Object.keys(val).forEach(function (key) {
-    out[key] = type.parse(val[key]);
-  });
-
-  return out;
-};
-
-function ClassType(type) {
-  this.type = type;
-  this.types = {};
-  this.optn = {};
-}
-
-ClassType.prototype.addRequired = function (key, type) {
-  this.types[key] = type;
-
-  return this;
-};
-
-ClassType.prototype.addOptional = function (key, type) {
-  this.optn[key] = type;
-
-  return this;
-}
-
-ClassType.prototype.parse = function (val) {
-  if (!(val instanceof this.type)) throw new Error();
-
-  var out = {};
-  Object.keys(this.types).forEach(function (key) {
-    out[key] = this.types[key].parse(val[key]);
-  }, this);
-
-  Object.keys(this.optn).forEach(function (key) {
-    if (val[key] === undefined) return;
-
-    out[key] = this.optn[key].parse(val[key]);
-  }, this);
-
-  return out;
-};
+var NumberType = proto.NumberType;
+var StringType = proto.StringType;
+var MapType    = proto.MapType;
+var ClassType  = proto.ClassType;
+var ArrayType  = proto.ArrayType;
 
 //-----
-var assert = require('assert');
 var type;
 
-type = new NumberType();
-assert.equal(type.parse(234), 234);
-assert.throws(function () {
-  type.parse('asdf');
-});
-assert.throws(function () {
-  type.parse()
-});
-
-type = new StringType();
-assert.equal(type.parse('hello'), 'hello')
-assert.throws(function () {
-  type.parse()
-});
-
-type = new ArrayType(type);
-type.parse(['hello'])
-assert.throws(function () {
-  type.parse([123])
+test(function (t) {
+  type = new NumberType();
+  t.equal(type.parse(234), 234);
+  t.throws(function () {
+    type.parse('asdf');
+  });
+  t.throws(function () {
+    type.parse()
+  });
+  t.end();
 })
 
-type = new MapType(new NumberType);
-assert.deepEqual(type.parse({'hi': 123}), {'hi':123});
-assert.throws(function () {
-  type.parse({'hi': 'asdf'})
+test(function (t) {
+  type = new StringType();
+  t.equal(type.parse('hello'), 'hello')
+  t.throws(function () {
+    type.parse()
+  });
+  t.end();
+})
+
+test(function (t) {
+  type = new ArrayType(type);
+  type.parse(['hello'])
+  t.throws(function () {
+    type.parse([123])
+  });
+  t.end();
+})
+
+test(function (t) {
+  type = new MapType(new NumberType);
+  t.deepEqual(type.parse({'hi': 123}), {'hi':123});
+  t.throws(function () {
+    type.parse({'hi': 'asdf'})
+  });
+  t.end();
 })
 
 function Man(m){
   this.name = m
 }
 
-type = new ClassType(Man);
-type.addRequired('name', new StringType)
-assert.deepEqual(type.parse(new Man('hi')), {'name': 'hi'})
-assert.throws(function () {
-  type.parse(new Man())
-})
-type.addOptional('age', new StringType)
-var m = new Man('hi')
-assert.deepEqual(type.parse(m), {name: 'hi'})
-m.age = 'asdf'
-assert.deepEqual(type.parse(m), {name: 'hi', age: 'asdf'})
+test(function (t) {
+  type = new ClassType(Man);
+  type.addRequired('name', new StringType)
+  t.deepEqual(type.parse(new Man('hi')), {'name': 'hi'})
+  t.throws(function () {
+    type.parse(new Man())
+  })
+  type.addOptional('age', new StringType)
+  var m = new Man('hi')
+  t.deepEqual(type.parse(m), {name: 'hi'})
+  m.age = 'asdf'
+  t.deepEqual(type.parse(m), {name: 'hi', age: 'asdf'})
+  t.end();
+});
 
-type = new MapType(new ClassType(Man).addRequired('name', new StringType))
-console.log(
-  type.parse({'as': new Man('hi')})
-)
-
-console.log('ok')
+test(function (t) {
+  type = new MapType(new ClassType(Man).addRequired('name', new StringType))
+  t.deepEqual(
+    type.parse({'as': new Man('hi')}),
+    {as: {name: 'hi'}}
+  );
+  t.end();
+});
